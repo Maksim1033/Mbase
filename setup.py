481d12d0419aafa3969099a967a467d159ecc22a
Maksim1033 <151359596+Maksim1033@users.noreply.github.com>
2025-09-27 21:14:27 +0700

    Старт

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="mbase",
     version="0.1.0",
     description="Удобная библиотека с базовыми инструментами",
-    author="Ваше Имя",
+    author="Maksim1033",
     packages=find_packages(),
-    install_requires=[],  # сюда добавьте сторонние зависимости, если есть
-    python_requires=">=3.6",
+    install_requires=[],
+    python_requires=">=3.1",
 )
